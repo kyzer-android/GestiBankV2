61fef90f5dca79addf45ebae59ff8e084d2229ae
kyzer-android <kyzer-android@gmail.com>
2019-10-09 14:13:42 +0200

    amdin crea agent+admin modif agent valid

--- webapp/gestibank/models/agents.py
+++ webapp/gestibank/models/agents.py
@@ -34,13 +34,12 @@ class Agent(User):
         }
         return test
 
-    #Liste les demandes de créations OK
-    def lister_demandecrea(self):
-        demandecrea = db.session.query(DemandeCreacompte).filter(DemandeCreacompte.affect == self.id).all()
-        list_demandecrea=[]
-        for demande in demandecrea:
-            list_demandecrea.append(demande.todict())
-        return list_demandecrea
+    def list_param(self):
+        return  ('id','username','nom','prenom','email','type','tel','debut_contrat')
+
+    # Retourne les demandes de création de compte avec le id agent OK
+    def filtre_compte(self):
+        return db.session.query(DemandeCreacompte).filter(DemandeCreacompte.affect == self.id).all()
 
     # Retourne les clients gérée par l'agent NOK
     def filtre_clients(self):
