e32b510f90b693af7b7002d9de5711775b98ce8a
kyzer-android <kyzer-android@gmail.com>
2019-10-10 11:43:15 +0200

    amdin crea agent+admin modif agent valid

--- webapp/gestibank/form.py
+++ webapp/gestibank/form.py
@@ -1,9 +1,12 @@
 from flask_babel import lazy_gettext as _l
-from flask_wtf import FlaskForm, widgets
-from wtforms import StringField, PasswordField, SubmitField, SelectMultipleField, RadioField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, RadioField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Email, Length, Optional, EqualTo
-
+from webapp.gestibank.models.admin import Admin
 #Formulaire de demande de creation de compte utilisateur
+
+
+
 class InscriptionForm(FlaskForm):
     username = StringField(_l("Connexion identifier"),validators=[Length(min=8, max=25)])
     password = PasswordField(_l("password"), validators=[Length(min=8, max=25)])
@@ -26,7 +29,7 @@ class AgentForm(FlaskForm):
     submit = SubmitField(_l('Send'))
 
 class ModifagentForm(FlaskForm):
-
+    delete = BooleanField(_l("Delete account"))
     username = StringField(_l("Connexion identifier"))
     nom = StringField(_l("FirstName"))
     prenom = StringField(_l("Surname"))
@@ -35,8 +38,14 @@ class ModifagentForm(FlaskForm):
     submit = SubmitField(_l('Send'))
 
 class ValidedemandFrom(FlaskForm):
-
     valide = RadioField("Valide", choices=[('True', 'Validation'),('False', 'En attente'),('None', 'Ne rien faire')])
+    submit = SubmitField(_l('Send'))
+
+
+
+class AffectdemandFrom(FlaskForm):
+
+    affect = SelectField(u'Programming Language')
     submit = SubmitField(_l('Send'))
 
 
--- webapp/tests/test_admin.py
+++ webapp/tests/test_admin.py
@@ -14,17 +14,34 @@ class TestUser (unittest.TestCase):
     def tearDown(self):
         db.session.remove()
 
-    def test_lister_agent(self):
-        test=Admin.lister_agent()
-        logging.info("test_lister_agent_param", test[0])
-        logging.info("test_lister_agent", test[1])
-        # for val in test :
-        #     logging.info("test_lister_un_agent",val)
+    # def test_lister_agent(self):
+    #     test=Admin.lister_agent()
+    #     logging.info("test_lister_agent_param", test[0])
+    #     logging.info("test_lister_agent", test[1])
+    #     # for val in test :
+    #     #     logging.info("test_lister_un_agent",val)
+    #
+    # def test_lister_demand(self):
+    #     utilisateur = Admin.query.get(1)
+    #     test=utilisateur.lister_demand_crea()
+    #     logging.info("lister les demande",test)
 
-    def test_lister_demand(self):
-        utilisateur = Admin.query.get(1)
-        test=utilisateur.lister_demand_crea()
-        logging.info("lister les demande",test)
+    # def test_list_demand(self):
+    #     listet = Admin.Choicelist_agent()
+    #     # final_list=[]
+    #     logging.info(listet)
+    #     # for list in listet:
+    #     #     final_list.append((list['id'],list['username']))
+    #
+    #     # logging.info("lister les demande", final_list)
+
+    def test_demande_crea_vide(self):
+        test = Admin.lister_demande_non_affecter()
+        logging.info("lister les demande", test)
+        if len(test) !=0:
+            logging.info("liste vide")
+        else :
+            logging.info("pas vide")
 
 
 if __name__ == "__main__":
