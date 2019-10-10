96c09ee63bc72826a4f602ce67bc3fc6d2c5e73b
kyzer-android <54835432+kyzer-android@users.noreply.github.com>
2019-10-10 17:15:46 +0200

    Add files via upload

---
+++ webapp/gestibank/models/transaction.py
@@ -0,0 +1,22 @@
+import enum
+
+from webapp import db
+
+
+class TypeOP(enum.Enum):
+    cb = 'CB'
+    cheque = 'chèque'
+    virement = 'virement'
+
+class transaction(db.Model):
+    id_operation = db.Column(db.Integer, primary_key=True)
+    Montant_operation=db.Column(db.Integer)
+    libeler_operation=db.Column(db.String(50))
+    nouveau_solde=db.Column(db.Integer)
+    type_operation=db.Column(db.Enum(TypeOP))
+    personne_tiers=db.Column(db.String(50))
+    id_compte = db.Column(db.Integer, db.ForeignKey('compte.id_compte'))
+
+
+
+
